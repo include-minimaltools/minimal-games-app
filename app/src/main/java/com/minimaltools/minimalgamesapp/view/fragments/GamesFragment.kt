866d8af051f1de2988fa5b844e6857abf44d7452
include-minimaltools <gabrielortizamador@gmail.com>
2022-05-02 18:24:01 -0600

    Navigation

---
+++ app/src/main/java/com/minimaltools/minimalgamesapp/view/fragments/GamesFragment.kt
@@ -0,0 +1,16 @@
+package com.minimaltools.minimalgamesapp.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.minimaltools.minimalgamesapp.R
+
+class GamesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = // Inflate the layout for this fragment
+        inflater.inflate(R.layout.fragment_games, container, false)
+}
---
+++ app/src/main/java/com/minimaltools/minimalgamesapp/view/fragments/VideoGameCompaniesFragment.kt
@@ -0,0 +1,16 @@
+package com.minimaltools.minimalgamesapp.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.minimaltools.minimalgamesapp.R
+
+class VideoGameCompaniesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = // Inflate the layout for this fragment
+        inflater.inflate(R.layout.fragment_video_game_companies, container, false)
+}
