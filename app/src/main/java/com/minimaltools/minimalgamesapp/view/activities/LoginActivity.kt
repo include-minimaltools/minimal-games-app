9ba7824e2ebc6897458db034f4beea3cc9fa0a9a
include-minimaltools <gabrielortizamador@gmail.com>
2022-05-04 02:54:35 +0000

    Login with Firebase Auth

--- app/src/main/java/com/minimaltools/minimalgamesapp/view/activities/LoginActivity.kt
+++ app/src/main/java/com/minimaltools/minimalgamesapp/view/activities/LoginActivity.kt
@@ -1,12 +1,75 @@
 package com.minimaltools.minimalgamesapp.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.minimaltools.minimalgamesapp.R
+import com.minimaltools.minimalgamesapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        with(binding) {
+            btnLogin.setOnClickListener {
+                try {
+                    val email = inputEmail.text.toString()
+                    val password = inputPassword.text.toString()
+
+                    if(validateInputs(email, password)) {
+                        login(email, password)
+                    }
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            setTheme(R.style.Theme_MinimalGamesApp)
+            setContentView(root)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else
+                    Toast.makeText(
+                        this@LoginActivity, getString(R.string.msg_user_password_incorrect),
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                binding.inputPassword.setText("") // clear password input
+            }
+    }
+
+    private fun validateInputs(email: String, password: String) : Boolean {
+        var valid = true
+        if (email.isEmpty()) {
+            binding.inputEmail.error = getString(R.string.msg_email_required)
+            valid = false
+        }
+
+        if (!email.contains("\\w+@\\w+\\.\\w+".toRegex())) {
+            binding.inputEmail.error = getString(R.string.msg_email_invalid)
+            valid = false
+        }
+
+        if (password.isEmpty()) {
+            binding.inputPassword.error = getString(R.string.msg_password_required)
+            valid = false
+        }
+
+        return valid
     }
 }
